0ac7adec175d9e341947699173e0acf2651d5b64
Amir <99111537+najibifar@users.noreply.github.com>
2025-09-11 16:48:54 +0330

    2025-11-09

---
+++ bokeh/thor3.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from bokeh.plotting import figure, output_file, show 
+from bokeh.models import ColumnDataSource
+from bokeh.models.tools import HoverTool
+from bokeh.palettes import Spectral5
+from bokeh.transform import factor_cmap
+output_file('thor2.html')
+
+data = pd.read_csv('thor_wwii.csv')
+data['MSNDATE'] = pd.to_datetime(data['MSNDATE'], format='%m/%d/%Y')
+dataGrouped = data.groupby('MSNDATE')[['TOTAL_TONS', 'TONS_FRAG','TONS_IC','TONS_HE']].sum()
+#print(dataGrouped)
+
+dataSource = ColumnDataSource(dataGrouped)
+p = figure(x_axis_type='datetime')
+p.line(x='MSNDATE', y='TOTAL_TONS', source=dataSource, legend_label='complet_explotion', line_width=2)
+
+show(p)
